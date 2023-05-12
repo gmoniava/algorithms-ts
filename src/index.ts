71a3f53105813bf563c4cf27c125beb61bda6d65
gmoniava <moniava.g@gmail.com>
2023-05-13 02:06:55 +0400

    minor change

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import generatePalindrome from "./generatePalindrome";
 
-const input = "a#b#c#b#a";
-const result = generatePalindrome(input);
-console.log(result);
+const input = "abc";
+generatePalindrome(input);
